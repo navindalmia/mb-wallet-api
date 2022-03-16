c95100a296822cf47464a8cf615d3951981cef04
navindalmia <navindalmia2003@yahoo.co.in>
2022-03-16 21:28:45 +0000

    Initial Commit

---
+++ mb-wallet-api/src/main/java/com/mb/api/mb_wallet_api/App.java
@@ -0,0 +1,13 @@
+package com.mb.api.mb_wallet_api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
